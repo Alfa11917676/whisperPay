e4ccb1c36d5b028ec799d8c65a49043f19afa4b5
Alfa11917676 <alfatitas747ray@gmail.com>
2025-11-23 00:27:56 +0530

    added dealer contract for L2

--- smartContracts/src/Dealer.sol
+++ smartContracts/src/Dealer.sol
@@ -1,12 +1,66 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { ReentrancyGuardUpgradeable as ReentrancyGuard } from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { Ownable2StepUpgradeable as Ownable } from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 import "./interface/IDealer.sol";
 
-contract Dealer is UUPSUpgradeable, ReentrancyGuard, IDealer {
-    constructor(){
+contract Dealer is UUPSUpgradeable, ReentrancyGuard, Ownable, IDealer {
 
+    using SafeERC20 for IERC20;
+
+    mapping (address => bytes32) private jobDetails;
+    mapping (bytes32 => bool) private jobStatus;
+
+    uint256 private nonce;
+    IERC20 public stableCoin;
+
+    //---------------------------------  Initializer  ----------------------------------------//
+    /// @notice Initializes the contract.
+    function initialize() external initializer onlyProxy {
+
+        __UUPSUpgradeable_init();
+        __Ownable2Step_init();
+        __Ownable_init(msg.sender);
+        __ReentrancyGuard_init();
+
+        nonce++;
+    }
+
+    /// @notice This function is used to deposit funds and transfer them anonymously
+    /// @param _backendDigest The backend message digest
+    /// @param _privateChainId The private chain id spined for this user
+    /// @param _totalAmount The total amount of funds to be transferred
+    function depositAndExec(
+        bytes calldata _backendDigest,
+        uint256 _privateChainId,
+        uint256 _totalAmount
+    ) external nonReentrant {
+
+        if (_privateChainId == 0)
+            revert InvalidChainId("Invalid ChainId");
+        if (_totalAmount == 0)
+            revert InvalidInteraction("Invalid Input Amount");
+
+        bytes32 jobDigest = keccak256(abi.encodePacked(_backendDigest, _privateChainId, _totalAmount, block.timestamp, nonce));
+        jobDetails[msg.sender] = jobDigest;
+        jobStatus[jobDigest] = false;
+        nonce++;
+        stableCoin.safeTransfer(address(this), _totalAmount);
+
+        emit L3Interaction(_backendDigest ,jobDigest, _privateChainId);
+    }
+
+
+
+    //---------------------------------  Internal Functions  ----------------------------------------//
+    /// @notice Authorizes the upgrade to a new implementation contract.
+    /// @param _implementation Address of the new implementation contract.
+    function _authorizeUpgrade(
+        address _implementation
+    ) internal virtual override onlyOwner {
+        if (_implementation.code.length <= 0) revert InvalidImplementation();
     }
 }
--- smartContracts/src/interface/IDealer.sol
+++ smartContracts/src/interface/IDealer.sol
@@ -3,4 +3,18 @@ pragma solidity ^0.8.10;
 
 interface IDealer {
 
+    event L3Interaction(bytes indexed _backendDigest, bytes32 _jobDigest, uint256 indexed _chainId);
+
+    error InvalidChainId(string reason);
+    error InvalidInteraction(string reason);
+    error InvalidImplementation();
+    error AuthenticationFailed(string reason);
+
+    function depositAndExec(
+        bytes calldata _backendDigest,
+        uint256 _privateChainId,
+        uint256 _totalAmount
+    ) external;
+
+
 }
